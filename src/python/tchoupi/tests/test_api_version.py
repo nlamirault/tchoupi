5adefa645aaab56bef36db859ae89bf5b2e3b127
Nicolas Lamirault <nicolas.lamirault@gmail.com>
2015-12-23 11:03:53 +0100

    FIX unit tests
    Signed-off-by: Nicolas Lamirault <nicolas.lamirault@gmail.com>

--- src/python/tchoupi/tests/test_api_version.py
+++ src/python/tchoupi/tests/test_api_version.py
@@ -15,29 +15,37 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-from flask import json
 import logging
-import tchoupi
 import unittest
+
+from flask import json
+
+from tchoupi import application
+
 
 logger = logging.getLogger(__name__)
 
 
 class TchoupiApiTestCase(unittest.TestCase):
 
-    def setUp(self):
-        self.app = tchoupi.app.test_client()
+    @classmethod
+    def setUpClass(cls):
+        cls.app = application.creates_app()
+        cls.app_client = cls.app.test_client()
+        cls.app_context = cls.app.test_request_context()
+        cls.app_context.push()
 
-    def tearDown(self):
+    @classmethod
+    def tearDownClass(self):
         pass
 
     def check_content_type(self, response):
-        logger.info("Response: %s %s" % (response.status, response.data))
-        self.assertEquals("application/json",
-                          response.headers.get("Content-Type"))
+        logging.info("Response: %s %s" % (response.status, response.data))
+        self.assertEqual("application/json",
+                         response.headers.get("Content-Type"))
 
     def http_get(self, uri):
-        return self.app.get(uri, follow_redirects=True)
+        return self.app_client.get(uri, follow_redirects=True)
 
 
 class ApiVersionTestCase(TchoupiApiTestCase):
@@ -45,6 +53,6 @@ class ApiVersionTestCase(TchoupiApiTestCase):
     def test_get_version_without_auth(self):
         response = self.http_get("/version")
         self.check_content_type(response)
-        self.assertEquals("200 OK", response.status)
+        self.assertEqual("200 OK", response.status)
         version = json.loads(response.data)
-        self.assertEqual('0.1.0', version['version'])
+        self.assertEqual('1', version['version'])
